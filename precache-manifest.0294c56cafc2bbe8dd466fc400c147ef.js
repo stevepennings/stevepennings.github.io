ec41efefd570c9b3c58de189a99046da6968c53b
Steve Pennings <stevepennings@outlook.com>
2019-12-15 16:20:18 +0100

    Updates

---
+++ precache-manifest.0294c56cafc2bbe8dd466fc400c147ef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f750e00dbf021944793f3b1a45a812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90f2ce901cb92c69a0c7",
+    "url": "/static/css/main.905bdd36.chunk.css"
+  },
+  {
+    "revision": "a05117749456b19e5c7d",
+    "url": "/static/js/2.b860a5d0.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.b860a5d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90f2ce901cb92c69a0c7",
+    "url": "/static/js/main.3f1d0846.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "4ecdea3b9e1d2f395e3e8d6fcd38a85d",
+    "url": "/static/media/Footer.4ecdea3b.svg"
+  },
+  {
+    "revision": "177b0593a59fc90d1dbd2165f1ba5719",
+    "url": "/static/media/Gallery.177b0593.svg"
+  },
+  {
+    "revision": "4aec0fe12e0be3a1fde3c534993a7883",
+    "url": "/static/media/backendIcon.4aec0fe1.svg"
+  },
+  {
+    "revision": "42f0636d0be26929ee4ae10b2ded1f4a",
+    "url": "/static/media/cardTopBackground.42f0636d.svg"
+  },
+  {
+    "revision": "654c644fc98b2d04a6a40be67f78c9b8",
+    "url": "/static/media/databasesIcon.654c644f.svg"
+  },
+  {
+    "revision": "93ecc53fe9184b0a8166d38625f43882",
+    "url": "/static/media/divider.93ecc53f.svg"
+  },
+  {
+    "revision": "a6f179989abff23f71ceb8e61bc124af",
+    "url": "/static/media/frontendIcon.a6f17998.svg"
+  },
+  {
+    "revision": "5a72e0b0b79fafda93bbc66ff10be0b2",
+    "url": "/static/media/illustrationIcon.5a72e0b0.svg"
+  },
+  {
+    "revision": "c215b8fea18b65c91ad85e05a58689bc",
+    "url": "/static/media/projectManagementIcon.c215b8fe.svg"
+  },
+  {
+    "revision": "5b58fd8b7ed0fdf0efbcc6a713c7f9c4",
+    "url": "/static/media/rocketBackground.5b58fd8b.svg"
+  },
+  {
+    "revision": "a4dab7de6bfdc677abf6f238a738f08e",
+    "url": "/static/media/uiuxIcon.a4dab7de.svg"
+  }
+]);
